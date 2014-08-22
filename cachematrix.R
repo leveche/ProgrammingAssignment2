896b4608b76b11efec52e69bb49e667967d157a1
Semyon Chaichenets <semyon@semyon.org>
2014-08-21 19:10:24 -0600

    assignment 2 - lexical scoping and closures

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,39 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+    # (cached) inverse
+    inv <- NULL
+
+    # methods to operate on the cached inverse
+    getinv <- function() inv
+    setinv <- function(matrixInverse) inv <<- matrixInverse
+    
+    ## getter and setter methods are boilerplate
+
+    get <- function() x
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    
+    # Return the handle to myself
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
+
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
 }
